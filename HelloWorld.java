f90db678c07d58b0bff9a0a943d27e92bf3d3822
Vernon Pitney <vernon.pitney@mrjaffesclass.com>
2022-09-30 11:29:32 -0700

    While Loops Complete

---
+++ HelloWorld.java
@@ -0,0 +1,34 @@
+public class HelloWorld
+{
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        int i = 0;
+        int k = 10;
+        int a = 0;
+        while (i < 15) {
+            System.out.println("i is " + i + " but is less than 15");
+            i = i + 1;
+        }
+        while (k > 5) {
+            System.out.println("k is " + k + " but is greater than 5");
+            k = k - 2;
+        }
+        while (a < 10 && b<= 20) {
+            System.out.println("a is " + a + " but is less than 10");
+            System.out.println("b is " + b + " but greater or equal to 20");
+            a = a + 1;
+        }
+    }
+    
+    
+    public static void main(String[] args)
+    {        
+        HelloWorld hw = new HelloWorld();
+        hw.run();
+    }
+}
