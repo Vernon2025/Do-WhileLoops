59d87c46dae0d7047eba9a63bab4e9b5f9d5d9df
Vernon Pitney <vernon.pitney@mrjaffesclass.com>
2022-10-16 21:13:15 -0700

    Do While

--- HelloWorld.java
+++ HelloWorld.java
@@ -10,19 +10,19 @@ public class HelloWorld
         int i = 0;
         int k = 10;
         int a = 0;
-        while (i < 15) {
+        do {
             System.out.println("i is " + i + " but is less than 15");
             i = i + 1;
-        }
-        while (k > 5) {
+        } while (i < 15);
+        do {
             System.out.println("k is " + k + " but is greater than 5");
             k = k - 2;
-        }
-        while (a < 10 && b<= 20) {
+        } while (k > 5);
+        do {
             System.out.println("a is " + a + " but is less than 10");
             System.out.println("b is " + b + " but greater or equal to 20");
             a = a + 1;
-        }
+        } while (a < 10 && b<= 20);
     }
     
     
